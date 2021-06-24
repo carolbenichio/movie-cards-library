47d5dbe401fe5c438419a78f1987ca180b5f144d
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-06-24 14:15:54 -0300

    Primeira estilizacao: mudanca de header, cards, list cards e rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -7,6 +7,7 @@ class Rating extends React.Component {
     const { rating } = this.props;
     return (
       <div className="movie-card-rating">
+        <p className='nota'>Nota:</p>
         <p className="rating">{ rating }</p>
       </div>
     );
